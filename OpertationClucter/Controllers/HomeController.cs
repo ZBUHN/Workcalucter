3135be5c2ede132d108a393763ee065896cdd77a
Zabnullah <z7777075995@gmail.com>
2023-05-18 14:49:31 +0300

    Final Commit

---
+++ OpertationClucter/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpertationClucter.Models;
+using System.Diagnostics;
+
+namespace OpertationClucter.Controllers
+{
+    public class HomeController : Controller
+    {
+        int va;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Calucter(int Number1, char Opertation, int Number2)
+        {
+            char Opertations = Opertation;
+            ViewData["Number1"] = Number1;
+            ViewData["Number2"] = Number2;
+            switch (Opertation)
+            {
+                case '+':
+                    va = Number1 + Number2;
+                    break;
+
+                case '-':
+                    va = Number1 - Number1;
+                    break;
+
+                case '*':
+                    va = Number1 * Number2;
+                    break;
+
+                case '/':
+                    va = Number1 / Number2;
+                    break;
+            }
+            ViewData["values"] = va;
+            ViewData["Opertation"] = Opertation;
+            return View("Index");
+        }
+
+
+    }
+}
